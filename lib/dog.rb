dff815f198f6ff7ffcd17526e144c6ba6265582a
Zach  Smith <zdsmith001@gmail.com>
2019-10-17 17:07:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,27 +26,54 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
-    INSERT INTO dogs (name, breed)
-    VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    if self.id
+      update
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    return self
   end
 
-  def self.create
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    dog = Dog.new(name: row[1], breed: row[2], id: row[0])
+    dog
   end
 
-  def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:, id)
+    if id
+      self.find_by_id(id)
+    else
+      self.create(name: name, breed: breed)
+    end
   end
 
-  def find_by_name
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
